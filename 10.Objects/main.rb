55d0516621bc0dd198a16a2a69ac74454c2f5d58
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2021-06-03 16:20:08 +0530

    Multiple Objects

---
+++ 10.Objects/main.rb
@@ -0,0 +1,19 @@
+class Main_Class
+
+def initialize(x, y, z)
+  
+
+@A = x
+@B = y
+@C= z
+
+puts "ID is: #@A"
+puts "Color is: #@B"
+puts "Name is: #@C"
+puts "\n"
+end
+end
+  
+
+obj1 = Main_Class. new(1,2,3)
+obj2 = Main_Class. new("One", "Two", "Three")
